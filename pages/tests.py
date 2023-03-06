760c004529eb5919251717d24c1fa665788e12d6
mitko <stoinovmitko2004@gmail.com>
2023-03-06 11:23:36 +0200

    Added tests

--- pages/tests.py
+++ pages/tests.py
@@ -1,15 +1,39 @@
-from django.test import TestCase
-
-# Create your tests here.
-
 from django.test import SimpleTestCase
+from django.urls import reverse
 
 
-class SimpleTests(SimpleTestCase):
-    def test_home_page_status_code(self):
-        response = self.client.get('/')
+class HomePageTests(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
 
-    def test_about_page_status_code(self):
-        response = self.client.get('/about/')
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):  # new
+        response = self.client.get(reverse("home"))
+        self.assertTemplateUsed(response, "home.html")
+
+    def test_template_content(self):  # new
+        response = self.client.get(reverse("home"))
+        self.assertContains(response, "<h1>Homepage</h1>")
+
+
+class AboutPageTests(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/about/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("about"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):  # new
+        response = self.client.get(reverse("about"))
+        self.assertTemplateUsed(response, "about.html")
+
+    def test_template_content(self):  # new
+        response = self.client.get(reverse("about"))
+        self.assertContains(response, "<h1>About page</h1>")
+        from django.test import SimpleTestCase
