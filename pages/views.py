d6d4c1ca5653632dee5e7dc2ef7e093a5b555500
mitko <stoinovmitko2004@gmail.com>
2023-03-28 11:51:12 +0300

    Added travis ci

--- pages/views.py
+++ pages/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Post
 
 
-class HomePageView(TemplateView):
-    template_name = 'home.html'
+class HomePageView(ListView):
+    model = Post
+    template_name = "home.html"
 
 
 class AboutPageView(TemplateView):
